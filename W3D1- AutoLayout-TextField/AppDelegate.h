5c89aa8214a5badf549b4267f1998376baa52831
dannylmathews <dannyl.mathews@gmail.com>
2015-01-25 18:36:40 -0800

    Initial Commit

---
+++ W3D1- AutoLayout-TextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W3D1- AutoLayout-TextField
+//
+//  Created by Daniel Mathews on 2015-01-25.
+//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ W3D1- AutoLayout-TextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W3D1- AutoLayout-TextField
+//
+//  Created by Daniel Mathews on 2015-01-25.
+//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
