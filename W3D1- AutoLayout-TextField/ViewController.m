9ac999860b23773b9b8045a7118692026ef4557f
dannylmathews <dannyl.mathews@gmail.com>
2015-03-22 19:11:39 -0700

    starting point2

--- W3D1- AutoLayout-TextField/ViewController.m
+++ W3D1- AutoLayout-TextField/ViewController.m
@@ -30,10 +30,10 @@
 
 - (void)updateViewConstraints {
     [super updateViewConstraints];
-    self.passwordYConstraint.constant = ([UIScreen mainScreen].bounds.size.height > 480.0f) ? 0 : -50;
-    
-    NSLog(@"%f = [UIScreen mainScreen].bounds.size.height",[UIScreen mainScreen].bounds.size.height);
-    NSLog(@"%f = [UIScreen mainScreen].bounds.size.width",[UIScreen mainScreen].bounds.size.width);
+//    self.passwordYConstraint.constant = ([UIScreen mainScreen].bounds.size.height > 480.0f) ? 0 : -50;
+//    
+//    NSLog(@"%f = [UIScreen mainScreen].bounds.size.height",[UIScreen mainScreen].bounds.size.height);
+//    NSLog(@"%f = [UIScreen mainScreen].bounds.size.width",[UIScreen mainScreen].bounds.size.width);
 
 }
 
@@ -43,7 +43,7 @@
         //The segue is to the a View Controller called NextViewController
         NextViewController *nvc =  (NextViewController  *) segue.destinationViewController;
         
-        nvc.nextLabel.text = @"Hello";
+        //nvc.nextLabel.text = @"Hello";
     }
 }
 
